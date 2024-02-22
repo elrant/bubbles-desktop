02c23a82f41468c04a66626b3a2168d2e40acf17
Luca <ludegiu@gmail.com>
2024-02-22 21:51:04 +0100

    put login-view.fxml in resources

--- src/main/java/team/elrant/bubbles/gui/LoginApplication.java
+++ src/main/java/team/elrant/bubbles/gui/LoginApplication.java
@@ -4,12 +4,13 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 
 public class LoginApplication extends Application {
     @Override
     public void start(@org.jetbrains.annotations.NotNull Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("login-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginApplication.class.getResource("/login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Login");
         stage.setScene(scene);
